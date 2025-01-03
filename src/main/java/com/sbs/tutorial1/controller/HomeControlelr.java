2ebd890b850d0a8c517074b97f1da8db0672619a
dup02090@naver.com <dup02090@naver.com>
2025-01-03 09:53:51 +0900

    home/main2

--- src/main/java/com/sbs/tutorial1/controller/HomeControlelr.java
+++ src/main/java/com/sbs/tutorial1/controller/HomeControlelr.java
@@ -13,7 +13,19 @@ public class HomeControlelr {
     //만약에 홈메핑 저게 요청이 들어오면 아래 멧드를 실행해줘
     @ResponseBody
             //아래 메서드를 실행한후 리턴값을 응답으로삼아서 바디에 출력해줘
+    //ResponseBody가 없으면 아래 메서드가 아닌 저 페이지로 이동
     public String showHome(){
         return "안녕";
+    }
+    @GetMapping("/home/main2")
+    @ResponseBody
+    public String showhome2(){
+        return"환영합니다";
+    }
+
+    @GetMapping("home/main3")
+    @ResponseBody
+    public String showhome3(){
+        return "나는 기상우 입니다.";
     }
 }
